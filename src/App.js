b9ffc796839f9abf26ebef197ef1d2e763ed155d
Angie Arrieta <andy.donoso1@gmail.com>
2022-02-24 14:29:58 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Redirect } from 'wouter'
+
+import ListOfDish from './components/ListOfDish';
+import NavBar from './components/NavBar';
+import ListOfIngredient from './components/ListOfIngredient';
+
+import DishDetail from './pages/DishDetail';
+import IngredientDetail from './pages/IngredientDetail';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return <div className="container w-100">
+        <NavBar />
+        <div className="container mx-auto mt-4">
+            <Route path='/dishes' component={ListOfDish} />
+            <Route path='/dish/:id' component={DishDetail} />
+
+            <Route path='/ingredients' component={ListOfIngredient} />
+            <Route path='/ingredient/:id' component={IngredientDetail} />
+            <Redirect to='/dishes' />
+        </div>
     </div>
-  );
 }
 
 export default App;
